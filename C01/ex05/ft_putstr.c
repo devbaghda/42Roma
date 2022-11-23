cfaa394f07f0a0f8470bb9214e15841984fca4b2
devbaghda <64898286+devbaghda@users.noreply.github.com>
2022-11-23 18:47:11 +0100

    added ex05, 06

---
+++ C01/ex05/ft_putstr.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: davbaghd <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/23 15:12:06 by davbaghd          #+#    #+#             */
+/*   Updated: 2022/11/23 18:21:30 by davbaghd         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+#include <stdio.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+	{
+		ft_putchar(*str);
+		str++;
+	}
+}
+
+int	main(void)
+{
+	char	*str;
+
+	str = "FANTASTICo";
+	ft_putstr(str);
+	return (0);
+}
