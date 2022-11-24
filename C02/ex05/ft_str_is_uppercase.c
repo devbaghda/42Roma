090b8e3b82dca13a3aaa0f94cf4a2a27e8d0b2f1
devbaghda <64898286+devbaghda@users.noreply.github.com>
2022-11-24 22:54:03 +0100

    added ex05

---
+++ C02/ex05/ft_str_is_uppercase.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_uppercase.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: davbaghd <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/24 22:34:33 by davbaghd          #+#    #+#             */
+/*   Updated: 2022/11/24 22:53:17 by davbaghd         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_str_is_alpha(char *str)
+{	
+	int	flag;
+	int	i;
+
+	i = 0;
+	if (!(*str))
+	{
+		flag = 1;
+	}
+	while (*str)
+	{
+		if (65 <= str[i] && str[i] <= 90)
+		{
+			flag = 1;
+		}
+		else
+		{
+			flag = 0;
+			break ;
+		}
+		str++;
+	}
+	return (flag);
+}
+
+int	main(void)
+{	
+	char	c;
+	char	*str;
+
+	str = &c;
+	str = "AAA";
+	ft_str_is_alpha(str);
+	printf("%d", ft_str_is_alpha(str));
+	return (0);
+}
