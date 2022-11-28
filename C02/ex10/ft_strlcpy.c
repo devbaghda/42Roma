483b987cde5c2524d2029b178768cd0368d2269d
devbaghda <64898286+devbaghda@users.noreply.github.com>
2022-11-28 17:55:15 +0100

    added ex10

---
+++ C02/ex10/ft_strlcpy.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: davbaghd <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/28 12:28:58 by davbaghd          #+#    #+#             */
+/*   Updated: 2022/11/28 17:54:24 by davbaghd         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	l;
+
+	i = 0;
+	l = 0;
+	while (i <= size - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	while (dest[l] != '\0')
+	{
+		l++;
+	}
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	char	c;
+	char	*dest;
+
+	dest = &c;
+	if (argc == 2)
+	{
+		ft_strlcpy(dest, argv[1], 2);
+	}
+	return (0);
+}
