5044cca12cba48eabb249e118680bbad55a53a5b
devbaghda <64898286+devbaghda@users.noreply.github.com>
2022-11-28 11:02:11 +0100

    Add files via upload
    added ex07

---
+++ C02/ex07/ft_strupcase.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strupcase.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: davbaghd <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/24 23:07:16 by davbaghd          #+#    #+#             */
+/*   Updated: 2022/11/28 11:01:14 by davbaghd         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_strupcase(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (97 <= str[i] && str[i] <= 122)
+		{
+			str[i] -= 32;
+			i++;
+		}
+		else
+		{
+		i++;
+		}
+	}
+	printf("%s", str);
+	return(str);
+
+}
+
+int main(int argc, char **argv)
+{
+	if (argc == 2)
+	{
+		ft_strupcase(argv[1]);
+	}	
+	return (0);
+}
