903a1b5e424aef4514f85d5d2080c6bed8ba8476
devbaghda <64898286+devbaghda@users.noreply.github.com>
2022-11-29 22:10:51 +0100

    added 04 -05

--- C03/ex04/ft_strstr.c
+++ C03/ex04/ft_strstr.c
@@ -6,8 +6,48 @@
 /*   By: davbaghd <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/29 00:38:54 by davbaghd          #+#    #+#             */
-/*   Updated: 2022/11/29 00:40:05 by davbaghd         ###   ########.fr       */
+/*   Updated: 2022/11/29 18:37:50 by davbaghd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <string.h>
 
+char	*ft_strstr(char *str, char *to_find)
+{
+	int i;
+	int j;
+	int k;	
+
+	i = 0;
+	j = 0;
+
+	if (!(*to_find))
+	{
+		return(str);
+	}
+	while (str[i] != '\0' && to_find[j] != '\0')
+	{
+		while (str[i] == to_find[j])
+		{
+			if (to_find[j + 1] == '\0')
+			{
+				return(&str[i - j]);
+			}
+			j++;
+			i++;
+		}
+		i++;
+	}
+	return (0);
+}
+
+int main(int argc, char **argv)
+{
+	if (argc == 3)
+	{
+		printf("%s\n", ft_strstr(argv[1], argv[2]));
+		printf("%s", strstr(argv[1], argv[2]));
+	}
+	return (0);
+}
---
+++ C03/ex05/ft_strlcat.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: davbaghd <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/29 18:42:09 by davbaghd          #+#    #+#             */
+/*   Updated: 2022/11/29 21:22:31 by davbaghd         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+
+unsigned int ft_strlen(char *str)
+{
+	unsigned int length;
+
+	length = 0;
+	while(str[length] != '\0')
+	{
+		length++;
+	}
+	return(length);
+}
+
+unsigned int ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int i;
+	unsigned int j;
+
+	i = ft_strlen(dest);
+	j = 0;
+	while (i + j < size)
+	{
+		dest[i + j] = src[j];
+		j++;
+	}
+	dest[i + j + 1] = '\0';
+	return (i + ft_strlen(src));
+}
+
+int main(void)
+{
+	char dest[20] = {"smth"};
+	char str2;
+	char *src;
+	unsigned int size;
+
+	src = &str2;
+	src = "effh";
+	size = 20;
+
+	printf("%d\n", ft_strlcat(dest, src, size));
+	printf("%s\n", dest);
+//    printf("%lu\n", strlcat(dest, src, size));
+//	printf("%s\n", dest);
+    return (0);
+}
