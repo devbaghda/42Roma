2d15e5672e0a6af825d673a1b6628be794b47cd8
devbaghda <64898286+devbaghda@users.noreply.github.com>
2022-12-05 20:52:48 +0100

    added 02

--- C06_wo_rep/ex02/ft_rev_params.c
+++ C06_wo_rep/ex02/ft_rev_params.c
@@ -1,38 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*   ft_rev_params.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: davbaghd <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/05 20:32:24 by davbaghd          #+#    #+#             */
-/*   Updated: 2022/12/05 20:41:50 by davbaghd         ###   ########.fr       */
+/*   Created: 2022/12/05 18:19:38 by davbaghd          #+#    #+#             */
+/*   Updated: 2022/12/05 20:31:08 by davbaghd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <unistd.h>
 
-void	ft_print_params(int n, char **str)
+void	ft_rev_params(int n, char **str)
 {
 	int	i;
 	int	j;
 
 	i = 1;
-	while (i < n)
+	while (n > 1)
 	{
 		j = 0;
-		while (str[i][j] != '\0')
+		while (str[n - 1][j] != '\0')
 		{
-			write(1, &str[i][j], 1);
+			write(1, &str[n - 1][j], 1);
 			j++;
 		}
 		write(1, "\n", 1);
-		i++;
+		n--;
 	}
 }
 
 int	main(int argc, char **argv)
 {
 	if (argc > 1)
-		ft_print_params(argc, argv);
+		ft_rev_params(argc, argv);
 	return (0);
 }
--- C06_wo_rep/ex03/ft_sort_params_v1.c
+++ C06_wo_rep/ex03/ft_sort_params_v1.c
@@ -5,16 +5,51 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: davbaghd <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/05 20:32:24 by davbaghd          #+#    #+#             */
-/*   Updated: 2022/12/05 20:41:50 by davbaghd         ###   ########.fr       */
+/*   Created: 2022/12/05 10:48:12 by davbaghd          #+#    #+#             */
+/*   Updated: 2022/12/05 20:44:28 by davbaghd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+#include <stdio.h>
 
-void	ft_print_params(int n, char **str)
+void ft_swap(char *a, char *b)
 {
-	int	i;
-	int	j;
+	char temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+
+void ft_sorting(int n, char **str)
+{
+	int i;
+	int j;
+
+	i = 1;
+	while (i < n) 
+	{
+		j = 0;
+		if (str[i + 1] != 0)
+		{
+			if (str[i] > str[i + 1])
+			{			
+				ft_swap(str[i], str[i + 1]);
+				j++;
+			}
+			
+		}
+		i++;
+	}
+}
+
+void ft_sort_params(int n, char **str)
+{
+	int i;
+	int j;
+
+	ft_sorting(n, str);
 
 	i = 1;
 	while (i < n)
@@ -30,9 +65,9 @@ void	ft_print_params(int n, char **str)
 	}
 }
 
-int	main(int argc, char **argv)
+int main (int argc, char **argv)
 {
 	if (argc > 1)
-		ft_print_params(argc, argv);
+		ft_sort_params(argc, argv);
 	return (0);
 }
