11a13a7fb21c18151d77a13677505cc959b32164
devbaghda <64898286+devbaghda@users.noreply.github.com>
2022-12-07 22:11:38 +0100

    03

--- C06_wo_rep/ex03/ft_sort_params.c
+++ C06_wo_rep/ex03/ft_sort_params.c
@@ -5,19 +5,37 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: davbaghd <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/05 20:32:24 by davbaghd          #+#    #+#             */
-/*   Updated: 2022/12/05 20:41:50 by davbaghd         ###   ########.fr       */
+/*   Created: 2022/12/07 13:40:44 by davbaghd          #+#    #+#             */
+/*   Updated: 2022/12/07 20:08:36 by davbaghd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <unistd.h>
 
-void	ft_print_params(int n, char **str)
+int	ft_strcmp(char *s1, char*s2)
+{	
+	int	i;
+
+	i = 0;
+	while (s1[i] != '\0' || s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
+	}
+	return (0);
+}
+
+void	ft_print_params(int argc, char **str)
 {
 	int	i;
 	int	j;
 
 	i = 1;
-	while (i < n)
+	while (i < argc)
 	{
 		j = 0;
 		while (str[i][j] != '\0')
@@ -30,9 +48,35 @@ void	ft_print_params(int n, char **str)
 	}
 }
 
+void	ft_sort_params(int argc, char **str)
+{
+	char	*temp;
+	int		i;
+	int		j;
+
+	i = 1;
+	while (i < argc - 1)
+	{
+		j = i + 1;
+		while (j < argc)
+		{
+			if (ft_strcmp(str[i], str[j]) > 0)
+			{
+				temp = str[i];
+				str[i] = str[j];
+				str[j] = temp;
+			}
+			j++;
+		}
+		j = 0;
+		i++;
+	}
+	ft_print_params(argc, str);
+}
+
 int	main(int argc, char **argv)
 {
 	if (argc > 1)
-		ft_print_params(argc, argv);
+		ft_sort_params(argc, argv);
 	return (0);
 }
